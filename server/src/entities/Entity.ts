bd5e27dcf26d0a885a1e946c27d25332b2a26b97
EungJae Lee <84191599+afkLee@users.noreply.github.com>
2022-11-30 14:08:38 +0900

    Update

--- server/src/entities/Entity.ts
+++ server/src/entities/Entity.ts
@@ -1,14 +1,14 @@
-import { Data } from "@google-cloud/logging/build/src/entry";
-import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+// import { Data } from "@google-cloud/logging/build/src/entry";
+// import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 
-export default abstract class Entity extends BaseEntity{
-    @PrimaryGeneratedColumn()
-    id: number;
+// export default abstract class Entity extends BaseEntity{
+//     @PrimaryGeneratedColumn()
+//     id: number;
 
-    @CreateDateColumn()
-    createdAt: Data;
+//     @CreateDateColumn()
+//     createdAt: Data;
 
-    @UpdateDateColumn()
-    updatedAt: Data;
-}
+//     @UpdateDateColumn()
+//     updatedAt: Data;
+// }
