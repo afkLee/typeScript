1c78c7588a9afabb74e3431c1b7fc899a51397fe
EungJae Lee <84191599+afkLee@users.noreply.github.com>
2022-12-04 00:05:00 +0900

    Add Vote_Entity

---
+++ server/src/entities/Vote.ts
@@ -0,0 +1,34 @@
+import { extname } from "path";
+import { Column, Entity, Index, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import User from "./User";
+import {Expose} from 'class-transformer';
+import Post from "./Post";
+
+import BaseEntity from './Entity';
+
+@Entity("votes")
+export default class Vote extends BaseEntity{
+    @Column()
+    value: number;
+
+    @ManyToOne(() => User)
+    @JoinColumn({name: "username",referencedColumnName:"username"})
+    user: User;
+
+
+
+    @Column()
+    username: string;
+
+    @Column({nullable:true})
+    postId: string;
+
+    @Column({nullable:true})
+    commentId: number;
+
+    @ManyToOne(() => Comment)
+    comment: Comment;
+
+
+
+}
